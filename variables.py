d63ca2612d85d00e05afd363da91145ff30910b9
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-28 21:34:47 +0530

    type() of variable

--- variables.py
+++ variables.py
@@ -3,5 +3,16 @@ age=23
 price=55.11
 print(name,age,price)
 print("My name is :",name)
+
+age2=age
+print(age2,age)
+# e1&r=12
+# print(e1&r)
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(age2))
+
 name=23
 print(name)
+print(type(name))
