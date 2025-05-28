eff7f711b63d78e00a838d7ebe3088f70bd39112
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-28 23:11:46 +0530

    Update inputinPython.py

--- inputinPython.py
+++ inputinPython.py
@@ -1,10 +1,10 @@
-a=input()
+a=input("enter the name:")
 print(a)
 print(type(a)) #input is always str 
 
 
-b=int(input())
+b=int(input("enter the number:"))
 print(b,type(b))  #int
 
-c=float(input())
+c=float(input("enter the float:"))
 print(b,type(c))  #float
