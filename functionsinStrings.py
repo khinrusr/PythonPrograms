861345dd45acf25219eb242bd4f4782469a76c96
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-29 00:19:31 +0530

    functionStrings

---
+++ functionsinStrings.py
@@ -0,0 +1,12 @@
+str="i am a am the am coder"
+
+print(str.endswith("er"))   #will give true if stri end with er
+
+print(str.capitalize())   #will make first word capital
+
+print(str.replace("a","b")) #will replace all a with b
+
+print(str.find("a"))  #will return first index of first occurence
+
+print(str.count("am"))  # will give count for the value 
+
