148ce01c7260b97f3564512ca4a68b58048ee286
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-28 23:40:06 +0530

    StringsOperations

--- practiceQuestions.py
+++ practiceQuestions.py
@@ -3,3 +3,14 @@
 a=int(input("Enter first number : "))
 b=int(input("Enter second number : "))
 print("Sum of Two Numbers : ",a+b)
+
+
+#input side of square and print its area
+side=float(input("Side of square : "))
+print("Area of square : ", side*side)
+print("Area of square : ", side**2)
+
+# take two numbers int and if greater or equals true if not then false
+a=int(input("Enter first number : "))
+b=int(input("Enter second number : "))
+print("Two numbers : ",(a>=b) )
---
+++ stringOperations.py
@@ -0,0 +1,9 @@
+str1="test the rule's for the match"
+str1='test the rule"s for the match'
+str1="""test the rule's for the match"s for thr test"""
+
+
+
+str2="rushi"
+str3="kesh"
+print(str2+str3)  #concatenation
