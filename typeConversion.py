2790f3fe80f03bd3d725473d2018dee041c12a28
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-28 23:00:56 +0530

    type casting

---
+++ typeConversion.py
@@ -0,0 +1,28 @@
+a=2
+b=5.2
+sum = a+b
+print(sum) #type of sum is float 
+
+
+"""
+a="2"
+b=5.2
+sum = a+b
+print(sum) #error can only concatenate str (not "float") to str
+"""
+
+"""
+a="2"
+b="5.5"
+sum=a+b
+print(sum) 
+
+"""
+
+# type casting
+
+a="2"
+b=5
+c=int(a)
+sum=c+b
+print(sum)
