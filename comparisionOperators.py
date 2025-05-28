7fad5d4df60773c0b2bd532c2d8cfcfedf25521a
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-28 22:35:35 +0530

    Create comparisionOperators.py

---
+++ comparisionOperators.py
@@ -0,0 +1,15 @@
+a=10
+b=15
+
+#equals
+print(a==b)
+#notequals
+print(a!=b)
+#samller than
+print(a<b)
+#greaterthan 
+print(a>b)
+#graeterthan equals
+print(a>=b)
+#smallerthan equals
+print(a<=b)
