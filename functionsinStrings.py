6cc99340f9b5ab336f9f45bd4a8eac12c5c9fffb
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-29 00:38:37 +0530

    practiceQuestionOnStringFunctions

--- functionsinStrings.py
+++ functionsinStrings.py
@@ -1,6 +1,9 @@
 str="i am a am the am coder"
 
-print(str.endswith("er"))   #will give true if stri end with er
+print(str.endswith("er"))   #will give true if string end with er
+
+ch="co"
+print(str.endswith(ch))   #will give true if string end with co
 
 print(str.capitalize())   #will make first word capital
 
@@ -10,3 +13,8 @@ print(str.find("a"))  #will return first index of first occurence
 
 print(str.count("am"))  # will give count for the value 
 
+print(str.startswith("er"))   #will give true if string tarts end with er
+
+print(str.startswith("i "))   #will give true if string starts with i 
+
+print(str.find("q")) #if its not present then will give -1
---
+++ practiceQuestionsOnStringFunctions.py
@@ -0,0 +1,6 @@
+# input users frinst name and print its length 
+name=input("Enter Your Frist Name : ")
+print("No. of letters in First Name :", len(name))
+
+# find to occurence of $ in the string
+print("Occurence of $ in the string :",name.count("$"))
