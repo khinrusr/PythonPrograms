912df45c7a044a6c431eb4220444a69a1a64d90e
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-28 23:50:58 +0530

    Indexing

---
+++ indexing.py
@@ -0,0 +1,6 @@
+str="rushikesh"
+print(str[0],str[7])
+a=str[2]
+print(type(a),a)
+
+# str[6]="a"     #not allowed error 'str' object does not support item assignment
--- stringOperations.py
+++ stringOperations.py
@@ -7,3 +7,8 @@ str1="""test the rule's for the match"s for thr test"""
 str2="rushi"
 str3="kesh"
 print(str2+str3)  #concatenation
+
+print(len(str1))  #for calculating lenth
+
+finalStr=str2+" "+str3
+print(finalStr)
