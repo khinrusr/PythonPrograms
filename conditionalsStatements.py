a079dbafe77e2c97db09c3a2009914ec01054472
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-29 01:01:41 +0530

    Update conditionalsStatements.py

--- conditionalsStatements.py
+++ conditionalsStatements.py
@@ -1,8 +1,26 @@
-lights = "yellow"
+lights = "blue"
 
 if(lights=="red"):
     print("Stop")
 elif(lights=="yellow"):
     print("Look")
-else:
+elif(lights=="green"):
     print("Go")
+else:
+    print("Lights are broken")
+print("End of code")
+
+
+
+#Grade students based on marks
+
+marks = float(input("Enter the Marks : "))
+
+if(marks>=90):
+    print("Grade = A")
+elif(90>marks>=80):
+    print("Grade = B")
+elif(80>marks>=70):
+    print("Grade = C")
+else:
+    print("Grade = D")
