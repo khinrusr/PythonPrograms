62ea9345debf9fca72b34fd1f2158922884d095c
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-28 22:53:53 +0530

    Create assignmentOperators.py

---
+++ assignmentOperators.py
@@ -0,0 +1,54 @@
+num=10
+print(num)
+num+=10
+print(num)
+num-=1
+print(num)
+num/=2
+print(num)
+num*=3
+print(num)
+num%=5
+print(num)
+num**=2
+
+# num=/6  not allowed
+print(num)
+
+
+# a +=40  not allowed need to declear first
+
+
+
+# Logical opreator 
+a=50
+b=10
+print(not True) #False
+
+print(not False) #true
+
+print(not (a>b))
+
+
+val1=True
+val2=True
+
+val3=True
+val4=False
+
+print("And Operator:", val1 and val2)
+
+print("And Operator:", val3 and val4)
+
+
+val5=True
+val6=True
+
+val7=True
+val8=False
+
+print("or Operator:", val5 or val6)
+
+print("or Operator:", val7 or val8)
+
+print("or Operator:", val4 or val8)
