3a59c5ec44d97b4f8b2e135fbd34a4eeca4a6314
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-29 00:14:10 +0530

    Update Slicing.py

--- Slicing.py
+++ Slicing.py
@@ -1,3 +1,15 @@
 str="rushikesh"
 a=str[1:5]
 print(a)
+print(str[3:],str[2:len(str)])
+print(str[2:len(str[5])]) #nothing will print str[5]this will give char
+print(str[-3:6])#nothing will print 
+
+print(str[:6])   #[0:6]
+
+# print(str[6:2]) does not work
+
+#negative slicking -ve index
+
+str = "rushikesh"
+print(str[-6:-1]) #hikes    
