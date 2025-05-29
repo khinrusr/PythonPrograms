f403214459d96967ecd10b43ebf9858ee7daae47
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-29 14:04:49 +0530

    Update practiceTestListAndTuples.py

--- practiceTestListAndTuples.py
+++ practiceTestListAndTuples.py
@@ -1,7 +1,7 @@
 # user enter three fav movie name and create its list
 
-list=[input("Enter Fav First Movie : "),input("Enter Fav Second Movie : "),input("Enter Fav Second Movie : ")]
-print(list)
+list3=[input("Enter Fav First Movie : "),input("Enter Fav Second Movie : "),input("Enter Fav Second Movie : ")]
+print(list3)
 
 #list contains palindrom of elements
 
@@ -35,3 +35,15 @@ if(list2==list2Copy):
     print("List is Palindrom")
 else:
     print("List is Not Palindrom")
+
+
+#count the no. of students with 'A' grade in the tuple
+
+tup=("C","D","A","A","B","B","A")
+print(tup.count("A"))
+
+# store the abpve value in list and sort them from "A" to "D"
+
+listSort=list(tup)
+listSort.sort()
+print(listSort)
