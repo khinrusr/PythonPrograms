a563e0963c28cc13d2a12eddae9373e9b4fc74f1
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-29 21:06:54 +0530

    practiceQuestionsDictionaryAndSet

---
+++ practiceQuestionsDictionaryAndSet.py
@@ -0,0 +1,37 @@
+#dictionary of floowing
+
+# table:"a piece of furniture", "list of facts & fig"
+# cat:"a samll animal"
+
+dic={
+    "table":["a piece of furniture","list of facts & fig"],
+    "cat":"a samll animal"
+}
+print(dic)
+
+
+#"python", "java", "C++", "python", "js", "java", "python","java","C++"","c"
+
+set1 = {"python", "java", "C++", "python", "js", "java", "python","java","C++","C"}
+
+print(len(set1))
+
+
+#
+dic1={}
+
+# dic1.update({input("Subject 1 name : "):input("Subject 1 marks : ")})
+# dic1.update({input("Subject 2 name : "):input("Subject 2 marks : ")})
+# dic1.update({input("Subject 3 name : "):input("Subject 3 marks : ")})
+
+
+# dic1[input("Subject 1 name : ")]=[input("Subject 1 marks : ")]
+# dic1[input("Subject 2 name : ")]=[input("Subject 2 marks : ")]
+# dic1[input("Subject 2 name : ")]=[input("Subject 2 marks : ")]
+
+print(dic1)
+
+
+set44={("int",9),("float",9.0)}
+set55={9,"9.0"}
+print(set44,set55)
