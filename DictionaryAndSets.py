170b78d781cd428902494e940e9afbfd58473a08
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-29 15:25:34 +0530

    DictionaryAndSets

---
+++ DictionaryAndSets.py
@@ -0,0 +1,18 @@
+info={
+    "name" : "Rushi",
+    "sub" : ["java","js","python"],
+    "topics" : ("str","int","float"),
+    "age" : 23,
+    "marks" : 97.99
+}
+
+print(info)
+
+print(info["name"],info["age"])
+
+info["age"]=25
+
+print(info)
+
+info["height"]=6.0
+print(info)
