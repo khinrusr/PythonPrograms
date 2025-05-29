49116c3b5f8e1f04c4ac08e3ebf0194426763bf5
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-29 10:09:28 +0530

    ListsAndTuples

---
+++ ListsAndTuples.py
@@ -0,0 +1,12 @@
+marks=[84,78,90,85]
+students=["rushi",'mohit','archana']
+lists=["rushi",56,99.999]
+
+print(marks,students,lists)
+print(type(marks),type(students),type(lists))
+print(len(marks),len(students),len(lists))
+
+
+marks[2]=95
+students[2]='Mau'
+print(marks,students,lists)
