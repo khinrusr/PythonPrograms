50e96b7694233c717e5f7e628a9238e5e0681d14
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-29 12:33:47 +0530

    methods of list

--- ListsAndTuples.py
+++ ListsAndTuples.py
@@ -1,3 +1,4 @@
+#list
 marks=[84,78,90,85]
 students=["rushi",'mohit','archana']
 lists=["rushi",56,99.999]
@@ -10,3 +11,34 @@ print(len(marks),len(students),len(lists))
 marks[2]=95
 students[2]='Mau'
 print(marks,students,lists)
+
+lists[1]="archana"
+print(lists)
+
+# lists[3]="test"  #list assignment index out of range
+# print(lists)
+
+#Slicing in list
+
+marks=[55,66,77,88,99,4,6,88,33]
+
+print(marks[2:4])
+print(marks[:3])
+print(marks[2:])
+print(marks[1:len(marks)])
+print(marks[-3:-1])
+print(marks[-4:-2])
+
+
+#methods of list
+list=[3,5,1,7,4,9]
+list.append(111)
+print(list) #add element at the end 
+list.sort()
+print(list) #sort in accending order
+list.sort(reverse=True)
+print(list) #soert in decending order 
+list.reverse()
+print(list)  #reverse the list
+list.insert(3,564)
+print(list)
