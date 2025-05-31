973cc2f2f3f4519444ff5c9346e93dafea226519
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-31 16:44:55 +0530

    Update range.py

--- range.py
+++ range.py
@@ -17,8 +17,8 @@
 #     print(el)
 
 #print the number from 100 to 1 
-for el in range(100,0,-1):
-    print(el)
+# for el in range(100,0,-1):
+#     print(el)
 
 # #multiplication table
 # x=4
@@ -26,5 +26,24 @@ for el in range(100,0,-1):
 #     print(x*el)
 
 
-for en in range(20):
-    pass
+# for en in range(20):
+#     pass
+
+
+# find the sum of first natural number 
+# n=int(input("number : "))
+# i=0
+# count=0
+# while i<=n:
+#     count=count+i
+#     i+=1
+# print(count)
+
+#find the factorial of first n number
+n=int(input("number : "))
+i=1
+count=1
+while i<=n:
+    count=count*i
+    i+=1
+print(count)
