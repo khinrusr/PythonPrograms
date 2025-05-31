45211e1687c0045b98c091a04dad40e308e23dfe
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-31 15:55:29 +0530

    range

---
+++ range.py
@@ -0,0 +1,12 @@
+#range()
+# for el in range(5):
+#     print(el)
+
+# for el in range(2,10):
+#     print(el)
+
+# for el in range(2,10,2): #print even number
+#     print(el)
+
+for el in range(1,10,2):  #print odd number
+    print(el)
