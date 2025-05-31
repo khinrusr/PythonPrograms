adabe90679af46a7636a86441c0e29bac5710313
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-31 15:48:17 +0530

    Update forLoop.py

--- forLoop.py
+++ forLoop.py
@@ -5,11 +5,33 @@
 # else:
 #     print("loop ended")
 
-str="Rushikesh"
-for val in str:
-    if(val=="k"):
-        print("K is found")
+# str="Rushikesh"
+# ele=input("Input the element : ")
+# for val in str:
+#     if(val==ele):
+#         print("K is found")
+#         break
+#     print(val)
+# else:
+#     print("Element not found")
+
+
+# print the element for the following list using for 
+# list=[1,4,9,16,25,36,49,64,81,100]
+
+# for el in list:
+#     print(el)
+
+
+# search for a element in tup
+tup=(1,4,9,16,25,36,49,64,81,100)
+ele=int(input("Input the element : "))
+idx=0
+for val in tup:
+    if(val==ele):
+        print(val,"is found at index",idx)
         break
+    idx+=1
     print(val)
 else:
-    print("loop ended")
+    print("Element not found")
