ada7327e086ac095941f08653f5e787336d4f1d5
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-31 15:27:33 +0530

    Create forLoop.py

---
+++ forLoop.py
@@ -0,0 +1,12 @@
+# list=[1,2,3,4,5,6,7,8,9,0]
+
+# for val in list:
+#     print(val)
+# else:
+#     print("loop ended")
+
+str="Rushikesh"
+for val in str:
+    print(val)
+else:
+    print("loop ended")
