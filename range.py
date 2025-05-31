6407988839d96ddd9181313fe711dd0768a1e576
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-05-31 16:10:03 +0530

    Update range.py

--- range.py
+++ range.py
@@ -8,5 +8,23 @@
 # for el in range(2,10,2): #print even number
 #     print(el)
 
-for el in range(1,10,2):  #print odd number
+# for el in range(1,10,2):  #print odd number
+#     print(el)
+
+
+#print num from 1 to 100
+# for el in range(101):
+#     print(el)
+
+#print the number from 100 to 1 
+for el in range(100,0,-1):
     print(el)
+
+# #multiplication table
+# x=4
+# for el in range(11):
+#     print(x*el)
+
+
+for en in range(20):
+    pass
