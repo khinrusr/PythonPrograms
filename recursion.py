9cfa3334fb76005fa741b20f4595e6381b7adefa
Rushikesh Khinde <122431129+khinrusr@users.noreply.github.com>
2025-06-01 12:17:16 +0530

    factorial

--- recursion.py
+++ recursion.py
@@ -1,7 +1,16 @@
-def show(n):
-    if(n==0):
-        return
-    print(n)
-    show(n-1)
+# def show(n):
+#     if(n==0):
+#         return
+#     print(n)
+#     show(n-1)
 
-show(5)
+# print(show(5))
+
+
+#factorial
+def factorial(n):
+    if (n==0 or n==1):
+        return 1
+    return n * factorial(n-1)
+
+print(factorial(9))
